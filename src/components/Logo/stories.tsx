bc2cd0435b390d21cd02b59f4259fbdb76735300
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-01 11:41:47 -0300

    feat(logo): create Logo component and update tests and storybook

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Logo from '.'
+
+export default {
+  title: 'components/Logo',
+  component: Logo
+} as Meta
+
+export const Default: StoryObj = {}
