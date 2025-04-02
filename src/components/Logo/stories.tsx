e28557ed19ec2591670073f58da3a1ee769eaee3
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-02 13:49:51 -0300

    chore(logo): adjust Storybook configuration for the component

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,38 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Logo from '.'
+import { Meta, StoryFn } from '@storybook/react'
+import Logo, { LogoProps } from '.'
 
 export default {
   title: 'components/Logo',
-  component: Logo
+  component: Logo,
+  argTypes: {
+    color: {
+      control: {
+        type: 'select'
+      },
+      options: ['white', 'black']
+    },
+    size: {
+      control: {
+        type: 'select'
+      },
+      options: ['normal', 'large']
+    },
+    hideOnMobile: {
+      control: {
+        type: 'boolean'
+      }
+    }
+  },
+  args: {
+    color: 'white',
+    size: 'normal',
+    hideOnMobile: false
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryFn<LogoProps> = (args) => <Logo {...args} />
