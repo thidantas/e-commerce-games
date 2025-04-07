16ce8080bebbedcd9a4fc9314a7747f1e722acee
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-07 17:51:34 -0300

    chore(menu): update stories to showcase open menu state on mobile

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,9 +1,16 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Menu from '.'
+import { Meta, StoryFn } from '@storybook/react'
+import Menu, { MenuProps } from '.'
 
 export default {
   title: 'components/Menu',
   component: Menu
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryFn<MenuProps> = (args) => <Menu {...args} />
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
