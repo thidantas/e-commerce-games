ea97e8da01c3fb2f88a9898edb1a28b4a2ade154
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-08 14:27:06 -0300

    chore(heading): update Storybook with new props

--- src/components/Heading/stories.tsx
+++ src/components/Heading/stories.tsx
@@ -1,4 +1,5 @@
-import { Meta, StoryFn } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
+
 import Heading, { HeadingProps } from '.'
 
 export default {
@@ -14,19 +15,34 @@ export default {
       },
       options: ['white', 'black']
     },
+    size: {
+      control: {
+        type: 'select'
+      },
+      options: ['medium', 'small']
+    },
+    lineColor: {
+      control: {
+        type: 'select'
+      },
+      options: ['primary', 'secondary']
+    },
     lineLeft: {
       type: 'boolean'
     },
     lineBottom: {
       type: 'boolean'
     }
-  },
-  args: {
-    children: 'Most Popular',
-    color: 'white',
-    lineLeft: false,
-    lineBottom: false
   }
 } as Meta
 
-export const Default: StoryFn<HeadingProps> = (args) => <Heading {...args} />
+export const Default: StoryObj<HeadingProps> = {}
+
+Default.args = {
+  children: 'Most Popular',
+  size: 'medium',
+  color: 'black',
+  lineLeft: false,
+  lineBottom: false,
+  lineColor: 'primary'
+}
