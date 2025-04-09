ed861ace1f60680a071fbcd299726dd52c2cedcf
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-09 12:59:59 -0300

    feat(button): enable polymorphism support with styled-components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,18 @@
-import { JSX } from 'react'
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, JSX } from 'react'
 
 import * as S from './styles'
+
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
 
 export type ButtonProps = {
   icon?: JSX.Element
   size?: 'small' | 'medium' | 'large'
   children?: React.ReactNode
   fullWidth?: boolean
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+} & ButtonTypes
 
 const Button = ({
   icon,
