212d3d342e92f24002f3d145a2b25c872bda89c4
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-09 17:33:56 -0300

    test(banner): add initial unit tests for component

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+
+import Banner from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+const props = {
+  img: 'https://imagemDeMock.com.br',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+  })
+})
