0f69d350528e12f23726aa9e960dd94578d28245
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-10 15:21:31 -0300

    chore(banner): update banner stories

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -5,11 +5,14 @@ export default {
   title: 'components/Banner',
   component: Banner,
   args: {
-    img: 'https://br.freepik.com/fotos-gratis/renderizacao-3d-de-fundo-de-textura-hexagonal_66626544.htm#fromView=keyword&page=1&position=0&uuid=d5d9cc39-e607-4b9a-9284-f010bfeb99b2&query=Papel+De+Parede',
+    img: 'https://img.freepik.com/fotos-gratis/renderizacao-3d-de-fundo-de-textura-hexagonal_23-2150796421.jpg?t=st=1744305474~exp=1744309074~hmac=21ca4e3043d63d9d289c195d50fbfca592080b8dad64dd27e9d56cf8be53d360&w=996',
     title: 'Defy death',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
   }
 } as Meta
 
