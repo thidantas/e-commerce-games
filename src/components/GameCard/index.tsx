17c8608ad9c4df0236aa1c925d2a0ef097fd261d
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-15 12:24:39 -0300

    feat(gamecard): create base structure and price display logic

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,47 @@
+import {
+  FavoriteBorder,
+  AddShoppingCart
+} from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+
+import * as S from './styles'
+
+export type GameCardProps = {
+  img: string
+  title: string
+  price: string
+  developer: string
+  promotionalPrice?: string
+}
+
+const GameCard = ({
+  img,
+  title,
+  price,
+  developer,
+  promotionalPrice
+}: GameCardProps) => (
+  <S.Wrapper>
+    <S.ImageBox>
+      <img src={img} alt={title} />
+    </S.ImageBox>
+
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton role="button">
+        <FavoriteBorder aria-label="Add to Wishlist" />
+      </S.FavButton>
+      <S.BuyBox>
+        {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+        <S.Price>{promotionalPrice || price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default GameCard
