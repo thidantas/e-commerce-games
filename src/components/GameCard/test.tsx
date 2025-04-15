c566f8bea9c8679f50a6803b83062c3330611230
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-15 12:50:55 -0300

    test(gamecard): add tests for price display and promotional logic

---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,55 @@
+import { screen } from '@testing-library/react'
+
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCard from '.'
+
+const mockProps = {
+  title: 'Borderlands 3',
+  developer: 'Gearbox Software',
+  img: '/img/borderlands-3-game-cover.png',
+  price: 'R$ 215,00'
+}
+
+describe('<GameCard />', () => {
+  it('should render the GameCard component', () => {
+    renderWithTheme(<GameCard {...mockProps} />)
+
+    expect(
+      screen.getByRole('heading', { name: mockProps.title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: mockProps.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: mockProps.title })).toHaveAttribute(
+      'src',
+      mockProps.img
+    )
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should render price in label', () => {
+    renderWithTheme(<GameCard {...mockProps} />)
+
+    const price = screen.getByText(mockProps.price)
+
+    expect(price).not.toHaveStyle({ textDecoration: 'line-through' })
+    expect(price).toHaveStyle({ backgroundColor: theme.colors.secondary })
+  })
+
+  it('should render a line-through in price when promotional', () => {
+    renderWithTheme(<GameCard {...mockProps} promotionalPrice="R$ 195,00" />)
+
+    expect(screen.getByText('R$ 215,00')).toHaveStyle({
+      textDecoration: 'line-through'
+    })
+
+    expect(screen.getByText('R$ 195,00')).not.toHaveStyle({
+      textDecoration: 'line-through'
+    })
+  })
+})
