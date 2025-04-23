da3f9b3405dd9a945bb0ce3c234917ec084bde9c
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-23 13:53:46 -0300

    test(slider): mock matchMedia in Jest for responsive behavior support

---
+++ .jest/mocks/matchMedia.ts
@@ -0,0 +1,14 @@
+export function mockMatchMedia() {
+  if (!window.matchMedia) {
+    window.matchMedia = (query: string) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn()
+    })
+  }
+}
