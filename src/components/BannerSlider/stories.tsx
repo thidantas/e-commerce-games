2d6f18459be523dcb61bddbea33d505b2c4ad7f2
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-24 18:20:11 -0300

    feat(banner-slider): create stories file for component

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryFn } from '@storybook/react'
+
+import BannerSlider, { BannerSliderProps } from '.'
+
+const items = [
+  {
+    img: '/img/resident-evil-1-img.jpg',
+    title: 'Resident Evil 1',
+    subtitle: "<p>Play the game & dive into the world of Spencer mansion's</p>",
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/resident-evil-1',
+    ribbonLabel: 'Best Selling'
+  },
+  {
+    img: '/img/resident-evil-2-img.jpg',
+    title: 'Resident Evil 2',
+    subtitle: '<p>Play the game & dive into the world of Raccoon city</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/resident-evil-2',
+    ribbonLabel: '20% OFF'
+  },
+  {
+    img: '/img/resident-evil-3-img.jpg',
+    title: 'Resident Evil 3',
+    subtitle: '<p>Play the game & dive into the world of Raccoon city</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/resident-evil-3',
+    ribbonLabel: '20% OFF'
+  }
+]
+
+export default {
+  title: 'components/BannerSlider',
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryFn<BannerSliderProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <BannerSlider {...args} />
+  </div>
+)
