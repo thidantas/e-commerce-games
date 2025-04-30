c5211abe06ce438376ce9a5fa62fd8edc177de09
thidantas <eng.thiagohpdantas@gmail.com>
2025-04-30 17:55:21 -0300

    test(home): add new unit tests to the Home page

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,14 +1,31 @@
 import { screen } from '@testing-library/react'
 
 import { renderWithTheme } from 'utils/tests/helpers'
+import gamesMock from 'components/GameCardSlider/mock'
+import bannerMock from 'components/BannerSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
 import Home from '.'
 
+const props = {
+  banners: bannerMock,
+  newGames: gamesMock,
+  mostPopularGames: gamesMock,
+  mostPopularHighlight: highlightMock,
+  upcommingGames: gamesMock,
+  upcommingHighlight: highlightMock,
+  upcommingMoreGames: gamesMock,
+  freeGames: gamesMock,
+  freeHighlight: highlightMock
+}
+
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    expect(screen.getAllByLabelText(/won games/i)).toHaveLength(2)
 
     expect(
       screen.getByRole('heading', { name: /contact us/i })
@@ -16,7 +33,7 @@ describe('<Home />', () => {
   })
 
   it('should render the sections', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /contact us/i })
@@ -33,5 +50,17 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
+  })
+
+  it('should render section elements', () => {
+    renderWithTheme(<Home {...props} />)
+
+    expect(screen.getAllByText(/Resident evil 1/i)).toHaveLength(1)
+
+    expect(
+      screen.getAllByText(/Prince of Persia - Sands of Time/i)
+    ).toHaveLength(5)
+
+    expect(screen.getAllByText(/read dead it's back/i)).toHaveLength(3)
   })
 })
