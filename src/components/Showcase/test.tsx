d2a057f32604ab3bcd4ee7acdd004de4be3a9edb
thidantas <eng.thiagohpdantas@gmail.com>
2025-06-04 14:59:07 -0300

    test(showcase): add unit tests for component

---
+++ src/components/Showcase/test.tsx
@@ -0,0 +1,70 @@
+import { render, screen } from '@testing-library/react'
+
+import Showcase from '.'
+import highlightMock from '../Highlight/mock'
+import gameCardSliderItemsMock from '../GameCardSlider/mock'
+import { renderWithTheme } from '@/utils/tests/helpers'
+import { GamesDimensions } from '@styled-icons/material-outlined/Games'
+
+const mockProps = {
+  title: 'Most Popular',
+  highlight: highlightMock,
+  games: gameCardSliderItemsMock.slice(0, 1)
+}
+
+describe('<Showcase />', () => {
+  it('should render full show case', () => {
+    renderWithTheme(<Showcase {...mockProps} />)
+
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: gameCardSliderItemsMock[0].title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: gameCardSliderItemsMock[0].title })
+    ).toBeInTheDocument()
+  })
+
+  it('should render without title', () => {
+    renderWithTheme(
+      <Showcase games={mockProps.games} highlight={mockProps.highlight} />
+    )
+
+    screen.getByRole('heading', { name: highlightMock.title })
+    screen.getByRole('heading', { name: gameCardSliderItemsMock[0].title })
+
+    expect(
+      screen.queryByRole('heading', { name: /most popular/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render without highlight', () => {
+    renderWithTheme(
+      <Showcase title={mockProps.title} games={mockProps.games} />
+    )
+
+    screen.getByRole('heading', { name: /most popular/i })
+    screen.getByRole('heading', { name: gameCardSliderItemsMock[0].title })
+
+    expect(
+      screen.queryByRole('heading', { name: highlightMock.title })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render without games', () => {
+    renderWithTheme(
+      <Showcase title={mockProps.title} highlight={mockProps.highlight} />
+    )
+
+    screen.getByRole('heading', { name: /most popular/i })
+    screen.getByRole('heading', { name: highlightMock.title })
+
+    expect(
+      screen.queryByRole('heading', { name: gameCardSliderItemsMock[0].title })
+    ).not.toBeInTheDocument()
+  })
+})
