03d1bea7a12a80e0dcefba7b2e735850296d9dd7
thidantas <eng.thiagohpdantas@gmail.com>
2025-06-12 17:35:38 -0300

    feat(GameDetails): develop component partially using TDD and implementation

---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,66 @@
+import { screen } from '@testing-library/react'
+
+import GameDetails, { GameDetailsProps } from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+const mockProps: GameDetailsProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'linux', 'mac'],
+  releaseDate: '2020-11-21T23:00:00',
+  rating: 'BR0',
+  genres: ['Role-playing', 'Action']
+}
+
+describe('<GameDetails />', () => {
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...mockProps} />)
+
+    expect(
+      screen.getByRole('heading', { name: /developer/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /release date/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /publisher/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
+  })
+
+  it('should render the formatted date', () => {
+    renderWithTheme(<GameDetails {...mockProps} />)
+
+    expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
+  })
+
+  it('should render free rating when BR0', () => {
+    renderWithTheme(<GameDetails {...mockProps} />)
+
+    expect(screen.getByText(/free/i)).toBeInTheDocument()
+  })
+
+  it('should render 18+ rating when BR18', () => {
+    renderWithTheme(<GameDetails {...mockProps} rating="BR18" />)
+
+    expect(screen.getByText(/18\+/i)).toBeInTheDocument()
+  })
+
+  it('should render a list of genres', () => {
+    renderWithTheme(<GameDetails {...mockProps} />)
+
+    expect(screen.getByText(/role-playing \/ action/i)).toBeInTheDocument()
+  })
+})
