0339461c2e647d8a2eb4a4885d13cc09bf802bde
thidantas <eng.thiagohpdantas@gmail.com>
2025-06-16 01:18:22 -0300

    feat(game-page): add initial logic for fetching dynamic slug data

--- src/app/game/[slug]/page.tsx
+++ src/app/game/[slug]/page.tsx
@@ -1,5 +1,33 @@
+import { notFound } from 'next/navigation'
 import Game from 'templates/Game'
 
-export default function Slug() {
-  return <Game />
+type SlugProps = {
+  params: {
+    slug: string
+  }
+}
+
+const dataMock = {
+  cyberpunk: {
+    cover:
+      'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg'
+  }
+}
+
+export async function generateStaticParams() {
+  return Object.keys(dataMock).map((slug) => ({
+    slug
+  }))
+}
+
+export default async function Slug({ params }: SlugProps) {
+  const { slug } = await params
+
+  const data = dataMock[slug as keyof typeof dataMock]
+
+  if (!data) {
+    notFound()
+  }
+
+  return <Game cover={data.cover} />
 }
