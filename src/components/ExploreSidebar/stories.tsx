c7b17e213db8e58e4f12c85446739a232c71b80b
thidantas <eng.thiagohpdantas@gmail.com>
2025-07-08 17:11:02 -0300

    feat(explore-sidebar): create initial structure of ExploreSidebar component

---
+++ src/components/ExploreSidebar/stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import ExploreSidebar from '.'
+
+export default {
+  title: 'components/ExploreSidebar',
+  component: ExploreSidebar,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj = {}
