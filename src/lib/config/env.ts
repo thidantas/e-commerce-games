7310d395a504c006a023986592977e8f80378d13
thidantas <eng.thiagohpdantas@gmail.com>
2025-07-27 15:53:14 -0300

    feat(env-config): create helper to access environment variables

---
+++ src/lib/config/env.ts
@@ -0,0 +1,12 @@
+function getEnvVar(key: string): string {
+  const value = process.env[key]
+
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`)
+  }
+  return value
+}
+
+export function getLocalApiUrl() {
+  return `http://${getEnvVar('LOCAL_HOST')}:${getEnvVar('LOCAL_PORT')}`
+}
