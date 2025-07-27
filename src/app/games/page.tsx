e4a9d4655b3d5ad123ed8f60af7a1559b009f68d
thidantas <eng.thiagohpdantas@gmail.com>
2025-07-27 17:37:56 -0300

    chore(games-page): update /games page to use getGames service for SSR

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,12 +1,17 @@
+import { getGames } from 'services/games/ssr/getGames'
+import { GameCardProps } from 'components/GameCard'
 import Games, { GamesProps } from 'templates/Games'
 import exploreSidebarItemsMock from 'components/ExploreSidebar/mock'
-import gameCardSliderItemsMock from 'components/GameCardSlider/mock'
+
+export const revalidate = 60
 
 const mockProps: GamesProps = {
-  games: gameCardSliderItemsMock,
+  games: [],
   filterItems: exploreSidebarItemsMock
 }
 
 export default async function GamesPage() {
-  return <Games {...mockProps} />
+  const games = await getGames({ limit: 9 })
+
+  return <Games {...mockProps} games={games as GameCardProps[]} />
 }
