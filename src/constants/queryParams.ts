1130319b151a72b82763ca5a50878496aea49599
thidantas <eng.thiagohpdantas@gmail.com>
2025-08-04 15:29:44 -0300

    chore(constants): add constants directory to centralize project-wide values

---
+++ src/constants/queryParams.ts
@@ -0,0 +1,6 @@
+export const QUERY_PARAMS = {
+  DATE: {
+    MOCKED_TODAY: '2021-01-21', // TODO: use this constant for now until the database is fully updated
+    TODAY: new Date().toISOString().slice(0, 10)
+  }
+}
