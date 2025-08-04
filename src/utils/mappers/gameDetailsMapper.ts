4886ab493c90abc0c5057abab49750c924230756
thidantas <eng.thiagohpdantas@gmail.com>
2025-08-04 15:18:39 -0300

    feat(utils): add formatPrice and new mappers for banner, games, game details, and highlight

---
+++ src/utils/mappers/gameDetailsMapper.ts
@@ -0,0 +1,29 @@
+import { env } from 'lib/config/env'
+import { GetGameBySlugQuery } from 'graphql/generated/graphql'
+
+export const gameDetailsMapper = (game: GetGameBySlugQuery['games'][0]) => {
+  return {
+    cover: game?.cover?.src
+      ? `${env.LOCAL_API_URL}${game?.cover?.src}`
+      : '/img/borderlands-3-game-cover.png',
+    gameInfo: {
+      title: game?.name || '',
+      price: game?.price ? game.price : 0,
+      description: game?.short_description || ''
+    },
+    gallery:
+      game?.gallery?.map((image) => ({
+        src: `${env.LOCAL_API_URL}${image?.src}`,
+        label: image?.label || ''
+      })) || [],
+    description: game?.description || '',
+    details: {
+      developer: game?.developers?.[0]?.name || 'Unknown developer',
+      releaseDate: game?.release_date || '',
+      platforms: game?.platforms?.map((platform) => platform?.name) || [],
+      publisher: game?.publisher?.name || '',
+      rating: game?.rating || '',
+      genres: game?.categories?.map((category) => category?.name) || []
+    }
+  }
+}
