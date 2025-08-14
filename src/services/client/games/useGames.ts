4f97d1949174ba040a107bef74ab7c9094baf796
thidantas <eng.thiagohpdantas@gmail.com>
2025-08-14 16:38:12 -0300

    refactor(services): update useGames service for client-side pagination

--- src/services/client/games/useGames.ts
+++ src/services/client/games/useGames.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { useQuery } from '@apollo/client'
 
 import { GET_GAMES } from 'graphql/queries/games/getGames'
@@ -5,16 +7,31 @@ import {
   GetGamesQuery,
   GetGamesQueryVariables
 } from 'graphql/generated/graphql'
+import { mapGamesDTO } from 'dtos/games/games.dto'
 
 export const useGames = (variables?: GetGamesQueryVariables) => {
-  const { data, loading, error } = useQuery<
+  const { data, loading, error, fetchMore } = useQuery<
     GetGamesQuery,
     GetGamesQueryVariables
   >(GET_GAMES, { variables })
 
+  const gamesDTO = data?.games ?? []
+
+  const handleFetchMore = () => {
+    return fetchMore({
+      variables: {
+        limit: 15,
+        start: gamesDTO?.length
+      }
+    })
+  }
+
+  const games = mapGamesDTO(gamesDTO)
+
   return {
-    games: data?.games || [],
+    data: games,
+    error,
     loading,
-    error
+    handleFetchMore
   }
 }
